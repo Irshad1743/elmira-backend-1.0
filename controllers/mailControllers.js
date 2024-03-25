244371119c231440a90e9824a95e505ba79d3942
Irshad <71060052+Irshad1743@users.noreply.github.com>
2024-03-25 11:43:17 +0530

    Update mailControllers.js

--- controllers/mailControllers.js
+++ controllers/mailControllers.js
@@ -5,9 +5,10 @@ exports.contactMail = async (req, res) => {
     try {
         const { name, email, phone, subject, message } = req.body;
         const bodytext = `Name: ${name.toUpperCase()} <br>Contact No.: ${phone} <br>Email: ${email.toUpperCase()} <br>Message: ${message.toUpperCase()}`;
+        const subjectDet = `ELMIRA BAGS CONTACT FORM FILLED BY ${name.toUpperCase()}`
         // mail module function calling
         try {
-            await mailModule(email, "elmira.contact@gmail.com", subject, bodytext, "Contact Us Form")
+            await mailModule(email, "elmira.contact@gmail.com", subjectDet, bodytext, "Contact Us Form")
             res.status(201).json({ message: "Mail sent successfully." });
         } catch (err) {
             res.status(422).json({ error: "Something went wrong while sending mail, try after some time." });
@@ -22,9 +23,10 @@ exports.warrantyMail = async (req, res) => {
     try {
         const { name, email, phone, orderid } = req.body;
         const bodytext = `Name: ${name.toUpperCase()} <br>Contact No.: ${phone} <br>Email: ${email.toUpperCase()} <br>Order ID: ${orderid.toUpperCase()}`;
+        const subjectDet = `ELMIRA BAGS WARRANTY REGISTERED BY ${name.toUpperCase()}`
         // mail module function calling
         try {
-            await mailModule(email, "elmira.contact@gmail.com", "Warranty Form Registration", bodytext, "Warranty Form")
+            await mailModule(email, "elmira.contact@gmail.com", subjectDet, bodytext, "Warranty Form")
             res.status(201).json({ message: "Mail sent successfully." });
         } catch (err) {
             res.status(422).json({ error: "Something went wrong while sending mail, try after some time." });
