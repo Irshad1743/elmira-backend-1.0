ed970e115167558b35e6bf023331eb08b3ad9f77
Irshad <71060052+Irshad1743@users.noreply.github.com>
2024-04-10 18:19:07 +0530

    Update mailModule.js

--- utils/mailModule.js
+++ utils/mailModule.js
@@ -13,7 +13,7 @@ const mailModule = (from, to, subject, bodytext, label) => {
     });
 
     const mailOptions = {
-        from: `"irshad" <${from}>`,
+        from: `irshadali.1743@gmail.com`,
         to: to,
         subject: `✅ ${subject}`,
         // text: "HI HELLO <h1> HELLO </h1>",
